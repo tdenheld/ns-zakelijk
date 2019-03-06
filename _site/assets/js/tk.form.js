44bcb9aa0b206d182fb3bb129def6021ae3deada
Tibor den Held <tibordenheld@Tibors-MacBook-Pro.local>
2019-03-06 20:20:02 +0100

    add slideDown to jquery validate

--- _site/assets/js/tk.form.js
+++ _site/assets/js/tk.form.js
@@ -3,12 +3,12 @@
 // ------------------------------------------------------------
 
 function form() {
-    var submit = $(".js-submit");
+    var submitLogin = $(".js-submit-login");
     var password = $("#js-toggle-password");
     var recoverPassword = $("#mz-recover-password");
 
-    if (submit[0]) {
-        submit.click(function () {
+    if (submitLogin[0]) {
+        submitLogin.click(function () {
             $(this).addClass("is-loading");
             $(".js-submit-loader-icon").addClass("is-active");
             setTimeout(() => {
@@ -36,16 +36,18 @@ function form() {
     };
 
     if (recoverPassword[0]) {
+        var errorSelector = ".js-formfield-error";
+
         recoverPassword.validate({
             onkeyup: false,
             errorClass: "is-error",
-            errorLabelContainer: ".js-formfield-error",
+            errorLabelContainer: errorSelector,
             messages: {
                 email: {
                     required: "Sorry, we hebben echt een e-mailadres van je nodig.",
                     email: "Kijk nog even. Dit e-mailadres lijkt niet correct."
                 }
-            },
+            }
         });
     };
 };
