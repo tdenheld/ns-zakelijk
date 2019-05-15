dcb89ed728669be26488f0a726c9094f764f0bdc
Tibor den Held <tibordenheld@static.kpn.net>
2019-05-15 16:03:40 +0200

    adding payment | loading

--- assets/js/ub.js
+++ assets/js/ub.js
@@ -22,7 +22,7 @@ function choiceSelector() {
         $(initID + " " + content).slideDown(0);
         $(initID).addClass(toggleClass);
 
-        $(obj).click(function () {            
+        $(obj).click(function () {
             if (!$(this).hasClass(toggleClass)) {
                 $(obj).removeClass(toggleClass);
                 $(this).addClass(toggleClass);
@@ -41,3 +41,35 @@ function choiceSelector() {
     };
 };
 choiceSelector();
+
+
+// full screen payment order check loader
+// ------------------------------------------------------------
+function payment() {
+    const btn = $(".js-bank");
+    const loader = ".js-bank-loader";
+    const loaderTxt = $(".js-loader-txt");
+    const bg = $(".js-header, .js-content, .js-footer");
+
+    function tween(x, t, b) {
+        TweenMax.to(x, t, {
+            ease: Power3.easeInOut,
+            opacity: 1,
+            filter: "blur(" + b + ")",
+            display: "block"
+        });
+    };
+
+    btn.click(function () {
+        let bank = $(".js-bank-set", this).text();
+        tween(loader, 0.2, 0);
+        tween(bg, 0.8, "10px");
+         setTimeout(() => {
+            loaderTxt.text("Verbinding maken met " + bank);
+        }, 2250);
+        setTimeout(() => {
+            window.location = "/uitgestelde-betaling/bedankt";
+        }, 4500);
+    });
+};
+payment();
