f9a52e26ef87e0f7455078abd511857418a6c58b
Tibor den Held <tibor.denheld@ns.nl>
2019-08-05 17:12:51 +0200

    update service | add map and cards

--- assets/js/zzp.js
+++ assets/js/zzp.js
@@ -16,7 +16,7 @@ function scrollMagic() {
         var customHook = $(obj).attr('hook');
         if (customHook != null) {
             hook = customHook;
-        };
+        }
         var scrll = new ScrollMagic.Scene({
                 triggerElement: obj,
             })
@@ -30,15 +30,44 @@ function scrollMagic() {
             })
             //.addIndicators()
             .addTo(controller);
-    };
+    }
 
     if (section[0]) {
         section.each(function (i) {
             scrollTrig(this);
         });
-    };
-};
+    }
+}
+
+function zzpService() {
+    const obj = $('.js-zzp-st');
+
+    if (obj[0]) {
+        obj.each(function () {
+            const header = $('.js-zzp-st-header', this);
+            const chevron = $('.js-zzp-st-chevron', this);
+            const close = $('.js-zzp-st-close', this);
+            const content = $('.js-zzp-st-content', this);
+
+            header.click(() => {
+                if (!header.hasClass('is-active')) {
+                    $('.js-zzp-st-header').removeClass('is-active');
+                    $('.js-zzp-st-chevron').show();
+                    $('.js-zzp-st-close, .js-zzp-st-content').hide();
+                    obj.css('z-index', '0');
+                }
+
+                $(this).css('z-index', '1');
+                content.toggle();
+                chevron.toggle();
+                close.toggle();
+                header.toggleClass('is-active');
+            });
+        });
+    }
+}
 
 $(function () {
     scrollMagic();
+    zzpService();
 });
