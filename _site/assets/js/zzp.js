c14f413db45a29b9a220ae5e8db0ca26fac1109f
Tibor den Held <tibor.denheld@ns.nl>
2019-08-09 15:22:59 +0200

    add service transition

--- _site/assets/js/zzp.js
+++ _site/assets/js/zzp.js
@@ -42,17 +42,59 @@ function scrollMagic() {
 function zzpService() {
     const obj = $('.js-zzp-st');
     const nav = $('.js-zzp-st-nav');
+    const index = 0;
+    let current;
 
     if (obj[0] && nav[0]) {
-        obj.eq(0).show();
-        nav.eq(0).addClass('is-active');
+        obj.eq(index).show();
+        nav.eq(index).addClass('is-active');
+
+        function transition(card, pos) {
+            let xEnd = -200;
+            let xStart = 200;
+
+            if (pos === 'prev') {
+                xEnd = 200;
+                xStart = -200;
+            }
+
+            const tl = new TimelineMax();
+            tl.to(obj, 0.3, {
+                ease: Power4.easeIn,
+                opacity: 0,
+                x: xEnd,
+                display: 'none'
+            }).fromTo(card, 0.4, {
+                x: xStart
+            },{
+                ease: Power4.easeOut,
+                opacity: 1,
+                x: 0,
+                display: 'block',
+            });
+        }
+
+        function checkIndex() {
+            nav.each(function () {
+                if ($(this).hasClass('is-active')) {
+                    current = $(this).parent().index();
+                }
+            });
+        }
 
         nav.each(function (i) {
+            const card = obj.eq(i);
+
             $(this).click(() => {
-                obj.hide();
+                checkIndex();
                 nav.removeClass('is-active');
                 $(this).addClass('is-active');
-                obj.eq(i).show();
+                
+                if (i > current) {
+                    transition(card);
+                } else if (i < current) {
+                    transition(card, 'prev');
+                }
             });
         });
     }
