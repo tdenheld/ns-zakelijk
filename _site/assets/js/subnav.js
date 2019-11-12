9ee9c2e93ad40c1190f6b92545b59731a9d35c65
Tibor den Held <tibor.denheld@ns.nl>
2019-11-12 09:06:34 +0100

    update subnav.js, flip bc/bk, remove border-top tile

--- _site/assets/js/subnav.js
+++ _site/assets/js/subnav.js
@@ -7,18 +7,18 @@
     const node = document.querySelector(obj);
     const header = document.querySelector('.js-header');
     const content = document.querySelector('.js-content');
+    const toggle = document.querySelector('.js-subnav-toggle');
+    const nav = ß('.js-subnav, .js-subnav-items, .js-subnav-toggle');
 
     header.style.position = 'absolute';
     content.style.marginTop = '120px';
 
     const scrolling = () => {
         (window.scrollY > 60) ? node.classList.add('is-sticky'): node.classList.remove('is-sticky');
+        nav.map((el) => el.classList.remove('is-active'));
     }
     scrolling();
+    
     window.addEventListener('scroll', () => requestAnimationFrame(scrolling));
-
-    ß('.js-subnav-toggle').map((el) => el.onclick = () => {
-        el.classList.toggle('is-active');
-        ß('.js-subnav, .js-subnav-items').map((el) => el.classList.toggle('is-active'));
-    });
+    toggle.onclick = () => nav.map((el) => el.classList.toggle('is-active'));
 })()
